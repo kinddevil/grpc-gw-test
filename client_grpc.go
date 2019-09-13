708f8775b403942c761c99327e473d916b5e2ea8
yichen <yichen@cas.com>
2019-09-13 21:40:53 +0800

    Revert "auto"
    This reverts commit 9377bb24db0856035a2f34ac27e9c137b3cee11b.

---
+++ client_grpc.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+	pb "grpc-gw-test/service_interfaces"
+	"log"
+	"time"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewSampleServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	ctx = metadata.AppendToOutgoingContext(ctx, "X-Meta-TrackId", "client-sent")
+
+	r, err := c.Sample(ctx, &pb.Request{Id: "1", Name: "anonymous"})
+	if err != nil {
+		log.Fatalf("could not score: %v", err)
+	}
+	log.Printf("Greeting: %v %v %v", r.Status, r.Code, r.Msg)
+}
--- servers/server.go
+++ servers/server.go
@@ -47,7 +47,7 @@ func StartServers() {
 	for i := len(servers) - 1; i >= 0; i-- {
 		server := servers[i]
 		if err := server.cancel; err != nil {
-			log.Printf("Stop server %v error %v", server.name, err())
+			log.Printf("Stop server %v error %v", server.name, err)
 		}
 	}
 }
