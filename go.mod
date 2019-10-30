b21c1fd599bf14188ea52be2bb549caf2f872f1e
yichen.wei <yichen.wei@hulu.com>
2019-10-30 20:14:03 +0800

    add prom

---
+++ go.mod
@@ -0,0 +1,26 @@
+module grpc-gw-test
+
+go 1.12
+
+require (
+	github.com/coreos/go-systemd v0.0.0-20190620071333-e64a0ec8b42a // indirect
+	github.com/gogo/protobuf v1.3.0 // indirect
+	github.com/golang/protobuf v1.3.2
+	github.com/google/uuid v1.1.1
+	github.com/grpc-ecosystem/go-grpc-middleware v1.0.1-0.20190118093823-f849b5445de4
+	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0
+	github.com/grpc-ecosystem/grpc-gateway v1.11.1
+	github.com/magiconair/properties v1.8.1 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/pelletier/go-toml v1.4.0 // indirect
+	github.com/prometheus/client_golang v1.2.1
+	github.com/spf13/afero v1.2.2 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/viper v1.4.0
+	go.etcd.io/etcd v0.0.0-20190917211250-838315283d9e
+	golang.org/x/net v0.0.0-20190912160710-24e19bdeb0f2 // indirect
+	golang.org/x/text v0.3.2 // indirect
+	google.golang.org/appengine v1.4.0 // indirect
+	google.golang.org/genproto v0.0.0-20190911173649-1774047e7e51
+	google.golang.org/grpc v1.23.1
+)
--- servers/grpc_server.go
+++ servers/grpc_server.go
@@ -2,16 +2,18 @@ package servers
 
 import (
 	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/spf13/viper"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/keepalive"
 	"net/http"
 
+	"github.com/grpc-ecosystem/go-grpc-middleware"
 	"github.com/grpc-ecosystem/go-grpc-prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"grpc-gw-test/cluster"
 	"grpc-gw-test/inspectors"
 	pb "grpc-gw-test/service_interfaces"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"grpc-gw-test/services"
 	"log"
 	"net"
@@ -45,15 +47,38 @@ func ServeGRPC(terminate chan<- CancelFun, cfgs *viper.Viper) {
 			Timeout:           time.Duration(timeOut) * time.Second,
 		}),
 		grpc.UnaryInterceptor(
-			inspectors.MiddlewareFunc(inspectors.GetUserInfo),
+			grpc_middleware.ChainUnaryServer(
+				inspectors.MiddlewareFunc(inspectors.GetUserInfo),
+				grpc_prometheus.UnaryServerInterceptor,
+			),
 		),
-		grpc.StreamInterceptor(grpc_prometheus.StreamServerInterceptor),
-		grpc.UnaryInterceptor(grpc_prometheus.UnaryServerInterceptor),
+		grpc.StreamInterceptor(
+			grpc_middleware.ChainStreamServer(
+				grpc_prometheus.StreamServerInterceptor,
+			),
+		),
 	)
 
 	// Register prometheus and open http port for scraping
+	// TODO abstract metrics logic
 	grpc_prometheus.Register(s)
 	http.Handle("/metrics", promhttp.Handler())
+	reg := prometheus.NewRegistry()
+	grpcMetrics := grpc_prometheus.NewServerMetrics()
+	customizedCounterMetric := prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "demo_server_say_hello_method_handle_count",
+		Help: "Total number of RPCs handled on the server.",
+	}, []string{"name"})
+	reg.MustRegister(grpcMetrics, customizedCounterMetric)
+	// TODO config port
+	httpServer := &http.Server{Handler: promhttp.HandlerFor(reg, promhttp.HandlerOpts{}), Addr: fmt.Sprintf("0.0.0.0:%d", 9092)}
+
+	go func() {
+		if err := httpServer.ListenAndServe(); err != nil {
+			log.Fatal("Unable to start a http server.")
+		}
+	}()
+	// TODO end
 
 	// Register service
 	registerServer := &cluster.RegisterService{}
@@ -62,6 +87,7 @@ func ServeGRPC(terminate chan<- CancelFun, cfgs *viper.Viper) {
 	terminate <- func() error {
 		registerServer.UnRegister(servName, addr)
 		s.Stop()
+		httpServer.Close()
 		return nil
 	}
 
